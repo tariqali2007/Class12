e4ecb7906b1debcdf3d2a6a8e1659af3a23fdcb2
tariqali2007 <tariqalimahar2007@gmail.com>
2024-06-19 22:14:34 +0500

    class12

---
+++ style12.js
@@ -0,0 +1,16 @@
+
+// synchronous vs asynchronous 
+
+ //      synchronous
+alert("hello pakistan ")
+console.log("hello students ");
+alert("hello pakistan1 ")
+console.log("hello world "); 
+
+
+// // asynchronous  
+//  setTimeout(function(){
+//       console.log("hello javascript ")
+//  }, 5000)
+
+//   console.log("helolo pakistan ");
